9a0f7139fa8ba8c8f17b6a7b423e729974af5927
krishkosle <79445977+krishkosle@users.noreply.github.com>
2022-01-21 15:43:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function preload() {}
+
+function setup() {
+    canvs = createCanvas(300, 300);
+    canv.center();
+
+}
+
+function draw() {
+
+
+}
+
+function take_snapshot() {
+    save('myFilterImage.png');
+
+}
